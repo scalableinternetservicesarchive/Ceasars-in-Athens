1a2217f16edb893e3190ce800bc047904156b0d1
kev737 <kevin.heffernan97@gmail.com>
2021-10-30 17:47:51 -0700

    kh - add migration for services

---
+++ db/migrate/20211031002700_create_services.rb
@@ -0,0 +1,13 @@
+class CreateServices < ActiveRecord::Migration[6.1]
+  def change
+    create_table :services do |t|
+      t.datetime :availability,  array: true, default: []
+      t.string :description
+      t.string :title
+      t.integer :zipcode
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
