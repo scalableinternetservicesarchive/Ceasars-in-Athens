3817e456629d212e425a777901670501087c184e
Ashley Wang <ashleyswang@ucsb.edu>
2021-11-09 14:41:46 -0800

    kh/aw -add router to services

--- cia/src/elements/services/Services.jsx
+++ cia/src/elements/services/Services.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import { withRouter } from '../../helpers/WithRouter'
 
 
 class Services extends Component {
     render(){
         // debugger
-        // console.log(this.props.match.params.id)
+        console.log(this.props.location)
         return(
             <h1>Hello World</h1>
         )
     }
 }
 
-export default Services
+export default withRouter(Services)
