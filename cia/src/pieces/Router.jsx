8b45cadf00d413945eede69b0be6a099e2fdb952
kev737 <kevin.heffernan97@gmail.com>
2021-11-09 13:22:54 -0800

    kh - add in scroll to top

--- cia/src/pieces/Router.jsx
+++ cia/src/pieces/Router.jsx
@@ -2,22 +2,24 @@ import React, { Component } from "react";
 import { Router, Routes, Route } from "react-router-dom";
 import Services from "./Services";
 import history from '../helpers/history';
+import ScrollToTop from "../helpers/ScrollToTop";
 
 
 export default class myRoutes extends Component {
+
     render() {
         return (
             <Router history={history}>
-              {/* <ScrollToTop> */}
+              <ScrollToTop>
                 <Routes>
-                    <Route path="/hi"><Services/></Route>
+                    <Route path="/hi" exact component={Services} />
                     {/* <Route path="/Posts"  component={Index} />
                     <Route path="/Post/:id" component={Post} />
                     <Route path='/spotify/callback' component={SpotifyCallback} />
                     <Route path="/login" component={Login} />
                     <Route path="/email" exact component={Email} /> */}
                 </Routes>
-              {/* </ScrollToTop> */}
+              </ScrollToTop>
             </Router>
         )
     }
