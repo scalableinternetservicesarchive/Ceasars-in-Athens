2b45af972ec13692f387bc3384e6ee4f6918dd3a
kev737 <kevin.heffernan97@gmail.com>
2021-11-08 21:05:15 -0800

    kh/pj - basic implementation

--- cia/src/index.jsx
+++ cia/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Services from './pieces/Services';
 
 ReactDOM.render(
   <React.StrictMode>
