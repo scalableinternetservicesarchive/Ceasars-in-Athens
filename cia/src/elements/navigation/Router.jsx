2fc083543641ef25104f3c6b0dd878060eed4fcf
Ashley Wang <ashleyswang@ucsb.edu>
2021-11-09 14:03:23 -0800

    kh/aw -add paths to components

--- cia/src/elements/navigation/Router.jsx
+++ cia/src/elements/navigation/Router.jsx
@@ -5,24 +5,21 @@ import history from '../../helpers/history';
 import Bookings from "../bookings/Bookings";
 
 export default class MyRoutes extends Component {
-    debugger
-    render() {
+  debugger
+  render() {
     console.log("hello")
 
-        return (
-            <Router history={history}>
-              {/* <ScrollToTop> */}
-                <Routes>
-                    <Route path="/hi" element={<Services />} />
-                    <Route path="/" element={<Bookings />} />
-                    {/* <Route path="/Posts"  component={Index} />
-                    <Route path="/Post/:id" component={Post} />
-                    <Route path='/spotify/callback' component={SpotifyCallback} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/email" exact component={Email} /> */}
-                </Routes>
-              {/* </ScrollToTop> */}
-            </Router>
-        )
-    }
+    return (
+      <Router history={history}>
+        {/* <ScrollToTop> */}
+        <Routes>
+          <Route path="/" element={<Services />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/bookings" element={<Bookings />} />
+        </Routes>
+        {/* </ScrollToTop> */}
+      </Router>
+    )
+  }
 }
