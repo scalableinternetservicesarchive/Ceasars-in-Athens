781b9d63e57f9ebe8db0c39b180052aba46b1ec3
Kerem Celik <kerem@signifai.io>
2021-11-18 18:02:46 -0800

    tsung wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('tsung_tests', 'users.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  user = User.new
+  user.id = row['id'].to_i
+  user.username = row['username']
+  user.password = row['password']
+  user.save
+end
