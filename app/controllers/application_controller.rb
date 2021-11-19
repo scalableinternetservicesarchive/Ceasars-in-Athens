c30947e8893c2f55b7798cf25138435003d7a65e
kev737 <kevin.heffernan97@gmail.com>
2021-11-18 21:40:56 -0800

    kh/pj - add headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
+# require 'pry'
 class ApplicationController < ActionController::Base
-  caches_page :show, :new
   include ApplicationHelper
   skip_forgery_protection
+
+  before_action :set_cache_headers
+
+  private
+
+  def set_cache_headers
+    # binding.pry
+    response.headers["Cache-Control"] = "public"
+    # response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Mon, 01 Jan 2022 00:00:00 GMT"
+  end
+
 end
